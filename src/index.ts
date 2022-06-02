97ab66fea3885e547a5949a719571b6958f69612
Rolando Arguello <rolo@Rolandos-MacBook-Pro-2.local>
2022-06-01 19:03:00 -0700

    trying to watch kyber prices

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import uniswapPool from './pools/uniswapPool';
 import kyberswapPool from './pools/kyberswapPool';
 const { mainnet: addresses } = require('./data/addresses');
 
-kyberswapPool(addresses.tokens.pools.DAI_ETH, addresses.kyber.kyberNetworkProxy);
+kyberswapPool(addresses.tokens.pools.USDC_ETH, addresses.kyber.kyberNetworkProxy);
--- src/pools/kyberswapPool.ts
+++ src/pools/kyberswapPool.ts
@@ -15,23 +15,16 @@ async function kyberswapPool(pool:any, kyberNetworkProxy:any) {
 
   let ethPrice:any;
   const updatePrice = async () => {
-    const results = await kyber
-      .methods
-      .getExpectedRate(
-        pool.tokenAAddress,
-        pool.tokenBAddress,
-        1
-      )
-      .call();
-
+    const results = await kyber.methods.getExpectedRate(pool.tokenAAddress, pool.tokenBAddress, 1).call();
     // We enter as param eth unit -> so we got how much wei It is
-    const ONE_ETH_WEI = web3.utils.toBN(web3.utils.toWei('1'));
+    const ONE_ETH_WEI = web3.utils.toBN(1000000);
     // Here we do the necessary conversion to have a readable ETH price
+    console.log(ONE_ETH_WEI);
     ethPrice = web3.utils.toBN('1').mul(web3.utils.toBN(results.expectedRate)).div(ONE_ETH_WEI);
     console.log(ethPrice.toString());
   }
+
   await updatePrice();
-  // Actualizamos la variable ethPrice cada 15 segundos
   setInterval(updatePrice, 15000);
 
 }
